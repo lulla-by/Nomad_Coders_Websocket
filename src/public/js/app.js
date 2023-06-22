7c59d581a113c487dacaf4643cf5d04b4a984998
lulla-by <thfl3363@hanmail.net>
2023-06-22 16:37:25 +0900

    socket.io를 통한 커스텀 이벤트 생성, payload발송,callback함수 실행

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,18 @@
 // io 함수는 알아서 socket.io를 실행하는 서버를 찾을 것
 const socket = io();
+
+//socket.io에는 기본적으로 room기능이 있음
+const welcome = document.getElementById("welcome")
+const form = welcome.querySelector("form")
+
+
+function handleRoomSubmit(e){
+e.preventDefault();
+const input = form.querySelector("input");
+// websocket에 비해 향상된 점
+// => 커스텀 이벤트 생성가능, 객체를 payload로 보낼 수 있음, 서버에서 호출하는 함수
+socket.emit("enter_room",{ payload:input.value },()=>{console.log("Server is done");})
+input.value="";
+}
+
+form.addEventListener("submit",handleRoomSubmit)
--- src/server.js
+++ src/server.js
@@ -18,7 +18,10 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection",socket=>{
-  console.log(socket);
+  // 커스텀 이벤트를 받아오고, 객체 출력이 가능, callback함수 실행
+  socket.on("enter_room",(msg,done)=>{console.log(msg),setTimeout(() => {
+    done()
+  }, 10000);})
 })
 httpServer.listen(3000, handleListen);
 
